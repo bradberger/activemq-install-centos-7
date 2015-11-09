1996979e80daa5586423f2ca99682599fabcf3f0
Brad Berger <brad@bradb.net>
2015-11-09 14:33:52 +0100

    Update install-activemq.sh

--- install-activemq.sh
+++ install-activemq.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd ~
+sudo yum install java-1.7.0-openjdk
 wget http://www.us.apache.org/dist/activemq/5.12.1/apache-activemq-5.12.1-bin.tar.gz
 md5sum apache-activemq*.tar.gz
 tar -zxvf apache-activemq*.tar.gz
-mv apache-activemq-5.12.1 /opt
-ln -s /opt/apache-activemq-5.12.1 /opt/activemq
-adduser --system activemq
-chown -R activemq: /opt/activemq
+sudo mv apache-activemq-5.12.1 /opt
+sudo ln -s /opt/apache-activemq-5.12.1 /opt/activemq
+sudo adduser --system activemq
+sudo chown -R activemq: /opt/activemq
