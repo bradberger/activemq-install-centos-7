5dd66df063fe8b282cc2256aa7f10e9aa1731fd6
Brad Berger <brad@bradb.net>
2015-11-10 16:35:29 +0100

    Update activemq.sh

--- etc/profile.d/activemq.sh
+++ etc/profile.d/activemq.sh
@@ -1,2 +1,3 @@
 export JAVA_HOME=/usr/lib/jvm/jre
 export PATH=$PATH:/opt/activemq/bin
+export ACTIVEMQ_SSL_OPTS = -Djavax.net.ssl.keyStore=/opt/activemq/conf/broker.ks -Djavax.net.ssl.keyStorePassword=password
